3e7b91c1e85efb8aa364f3c22c0d9658a84b2b4f
deepakbablu <47096784+deepakbablu@users.noreply.github.com>
2023-10-16 07:40:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker, This is for demo</h2>'
+    return '<h1>Hello from Flask & Docker d, This is for demo</h2>'
 
 
 if __name__ == "__main__":
